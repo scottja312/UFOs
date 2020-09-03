bc41c61814ae153f441b2a40e7336864206f2d64
scottja312 <64980711+scottja312@users.noreply.github.com>
2020-09-03 17:42:44 -0400

    updated app.js with table code.

--- js/app.js
+++ js/app.js
@@ -3,3 +3,23 @@ const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+// Build table to hold js data and clear existing data.
+function buildTable(data){
+    tbody.html("");
+
+// Next, loop through each object in the data
+// and append a row and cells for each value in the row
+data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });   
+}
